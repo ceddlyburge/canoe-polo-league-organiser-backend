e6a69002ab05558d5ef774e8da62139c886b305a
CeddlyBurge <ceddlyburge@gmail.com>
2018-01-23 20:32:24 +0000

    Restore fast but complicated MaxConsecutiveMatchesByAnyTeam and add tests for both the slow and fast / complicated version

--- CanoePoloLeagueOrganiser/MaxConsecutiveMatchesByAnyTeam.cs
+++ CanoePoloLeagueOrganiser/MaxConsecutiveMatchesByAnyTeam.cs
@@ -8,7 +8,7 @@ using static System.Math;
 
 namespace CanoePoloLeagueOrganiser
 {
-    public class MaxConsecutiveMatchesByAnyTeam
+    public class MaxConsecutiveMatchesByAnyTeamSlowButObvious
     {
         GameList GameList { get; set; }
 
@@ -34,4 +34,33 @@ namespace CanoePoloLeagueOrganiser
                 .TakeWhile(game => game.Playing(team))
                 .Count();
     }
+
+    public class MaxConsecutiveMatchesByAnyTeam
+    {
+        public uint Calculate(GameList games)
+        {
+            Requires(games != null);
+
+            uint maxConsecutiveGames = 1;
+            uint lastHomeTeamConsecutiveGames = 0;
+            uint lastAwayTeamConsecutiveGames = 0;
+            string lastHomeTeam = null;
+            string lastAwayTeam = null;
+
+            foreach (var game in games.Games)
+            {
+                lastHomeTeamConsecutiveGames = (game.Playing(lastHomeTeam) == true) ? lastHomeTeamConsecutiveGames + 1 : 1;
+                lastAwayTeamConsecutiveGames = (game.Playing(lastAwayTeam) == true) ? lastAwayTeamConsecutiveGames + 1 : 1;
+
+                maxConsecutiveGames = (maxConsecutiveGames < lastHomeTeamConsecutiveGames) ? lastHomeTeamConsecutiveGames : maxConsecutiveGames;
+                maxConsecutiveGames = (maxConsecutiveGames < lastAwayTeamConsecutiveGames) ? lastAwayTeamConsecutiveGames : maxConsecutiveGames;
+
+                lastHomeTeam = game.HomeTeam.Name;
+                lastAwayTeam = game.AwayTeam.Name;
+            }
+
+            return maxConsecutiveGames;
+        }
+    }
+
 }
---
+++ CanoePoloLeagueOrganiserTests/MaxConsecutiveMatchesByAnyTeamTests.cs
@@ -0,0 +1,66 @@
+﻿using CanoePoloLeagueOrganiser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CanoePoloLeagueOrganiserTests
+{
+    public class MaxConsecutiveMatchesByAnyTeamTests
+    {
+        [Fact]
+        public void TwoInRowAtStart()
+        {
+            var games = new GameList(
+                new List<Game> {
+                 new Game("Castle", "Battersea"),
+                 new Game("Avon", "Castle"),
+             });
+
+            var slow = new MaxConsecutiveMatchesByAnyTeamSlowButObvious().Calculate(games);
+            var fast = new MaxConsecutiveMatchesByAnyTeam().Calculate(games);
+
+            Assert.Equal((uint) 2, slow);
+            Assert.Equal((uint) 2, fast);
+        }
+
+        [Fact]
+        public void TwoInRowAtEnd()
+        {
+            var games = new GameList(
+                new List<Game> {
+                 new Game("Braintree", "MAD"),
+                 new Game("Castle", "Battersea"),
+                 new Game("Avon", "Castle"),
+             });
+
+            var slow = new MaxConsecutiveMatchesByAnyTeamSlowButObvious().Calculate(games);
+            var fast = new MaxConsecutiveMatchesByAnyTeam().Calculate(games);
+
+            Assert.Equal((uint)2, slow);
+            Assert.Equal((uint)2, fast);
+        }
+
+        [Fact]
+        public void ThreeInRowInMiddle()
+        {
+            var games = new GameList(
+                new List<Game> {
+                 new Game("Braintree", "MAD"),
+                 new Game("Castle", "Battersea"),
+                 new Game("Avon", "Castle"),
+                 new Game("Avon", "Castle"),
+                 new Game("Braintree", "MAD"),
+             });
+
+            var slow = new MaxConsecutiveMatchesByAnyTeamSlowButObvious().Calculate(games);
+            var fast = new MaxConsecutiveMatchesByAnyTeam().Calculate(games);
+
+            Assert.Equal((uint)3, slow);
+            Assert.Equal((uint)3, fast);
+        }
+
+    }
+}
