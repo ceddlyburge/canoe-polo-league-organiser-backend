27fa5a0407fe6e4410c4bb3725b1a90b9afc8332
CeddlyBurge <ceddlyburge@gmail.com>
2018-02-22 18:35:27 +0000

    Compress playlistexception

--- CanoePoloLeagueOrganiser/PlayListException.cs
+++ CanoePoloLeagueOrganiser/PlayListException.cs
@@ -4,16 +4,10 @@ namespace CanoePoloLeagueOrganiser
 {
     public class PlayListException : Exception
     {
-        public PlayListException()
-        {
-        }
+        public PlayListException() { }
 
-        public PlayListException(string message) : base(message)
-        {
-        }
+        public PlayListException(string message) : base(message) { }
 
-        public PlayListException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
+        public PlayListException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
