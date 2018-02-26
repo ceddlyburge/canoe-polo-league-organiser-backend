1ecc1c5d7b6c7a4d7dee23d0e682b7057807b0a5
CeddlyBurge <ceddlyburge@gmail.com>
2018-02-26 07:53:17 +0000

    Split partial game order metrics class in to own file

--- CanoePoloLeagueOrganiser/GameOrderMetrics.cs
+++ CanoePoloLeagueOrganiser/GameOrderMetrics.cs
@@ -2,13 +2,6 @@
 
 namespace CanoePoloLeagueOrganiser
 {
-    public class PartialGameOrderMetrics
-    {
-        public uint? OccurencesOfTeamsPlayingConsecutiveMatches { get; set; }
-        public uint? MaxConsecutiveMatchesByAnyTeam { get; set; }
-        public uint? GamesNotPlayedBetweenFirstAndLast { get; set; }
-    }
-
     public class GameOrderMetrics
     {
         public uint OccurencesOfTeamsPlayingConsecutiveMatches { get; set; }
---
+++ CanoePoloLeagueOrganiser/PartialGameOrderMetrics.cs
@@ -0,0 +1,9 @@
+﻿namespace CanoePoloLeagueOrganiser
+{
+    public class PartialGameOrderMetrics
+    {
+        public uint? OccurencesOfTeamsPlayingConsecutiveMatches { get; set; }
+        public uint? MaxConsecutiveMatchesByAnyTeam { get; set; }
+        public uint? GamesNotPlayedBetweenFirstAndLast { get; set; }
+    }
+}
