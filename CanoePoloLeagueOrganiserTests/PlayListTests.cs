6803fb94c63de8ac7d8daa5aaa0609444ae28dfe
CeddlyBurge <ceddlyburge@gmail.com>
2018-02-28 08:01:13 +0000

    Add PlayListTests

---
+++ CanoePoloLeagueOrganiserTests/PlayListTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using CanoePoloLeagueOrganiser;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CanoePoloLeagueOrganiserTests
+{
+    public class PlayListTests
+    {
+        [Fact]
+        public void Teams()
+        {
+            var sut = CreatePlaylist(
+                new Game("Clapham", "Castle"),
+                new Game("Clapham", "Castle"),
+                new Game("Battersea", "VKC")
+                );
+
+            var teams = sut.Teams.Select(t => t.ToString()).ToList();
+
+            Assert.Equal(4, teams.Count);
+            Assert.Contains("Clapham", teams);
+            Assert.Contains("Castle", teams);
+            Assert.Contains("Battersea", teams);
+            Assert.Contains("VKC", teams);
+        }
+
+        static PlayList CreatePlaylist(params Game[] games) =>
+            new PlayList(games.ToList());
+
+    }
+}
