fccf69ca6fee4708508ad4197f961fb9b520752e
CeddlyBurge <ceddlyburge@gmail.com>
2018-03-15 18:22:06 +0000

    Add Team tests

---
+++ CanoePoloLeagueOrganiserTests/TeamTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CanoePoloLeagueOrganiser;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CanoePoloLeagueOrganiserTests
+{
+    public class TeamTests
+    {
+        [Fact]
+        public void EqualsDifferentCase()
+        {
+            var sut = new Team("Clapham");
+
+            Assert.True(sut.Equals(new Team("clapham")));
+        }
+
+        [Fact]
+        public void EqualsString()
+        {
+            var sut = new Team("Clapham");
+
+            Assert.False(sut.Equals("clapham"));
+        }
+    }
+}
